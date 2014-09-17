dfd3d1501771423559fe6f53aed269aa1a3b5600
IzzyRahaman <izzyrahaman@gmail.com>
2014-09-16 20:18:52 -0400

    Init repo

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name assignment01bApp
+ * @description
+ * # assignment01bApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('assignment01bApp', []);
